720c6f5dcc3a5dc08fb9cd0101aa921f6e85b05c
Miguel Muñoz <mmestelles@gmail.com>
2022-02-05 16:51:56 +0100

    array of favourites building properly. However, there are duplicate ids when checking in different sections. I have to rethink how to add all resources to favourites array without conflicts with ids

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { AppProvider } from 'context/AppContext'
 import Searchbar from 'components/Layout/SearchBar/Searchbar'
 import Navbar from 'components/Layout/NavBar/Navbar'
 import Home from 'pages/Home/Home'
@@ -10,6 +11,8 @@ import './App.css'
 function App() {
   const [search, setSearch] = useState('')
   const [click, setClick] = useState(true)
+  const [favorites, setFavorites] = useState([])
+  console.log(favorites);
 
   //SearchBar
   const getInfo = (searchValue) => {
@@ -21,22 +24,43 @@ function App() {
     setClick(clicked)
   }
 
+  //Add Favorite
+  const addFavorite = (id) => {
+    let copy = [...favorites]
+    copy.push(id)
+    setFavorites(copy)
+  }
+
+  //Delete Favorite 
+  const deleteFavorite = (id) => {
+    let copy = [...favorites]
+    copy.splice(copy.indexOf(id), 1)
+    setFavorites(copy)
+  }
+
   return (
     <div className="App">
       <div className='App-container'>
-        <div className='App-search'>
-          <Searchbar getInfo={getInfo} getClick={getClick} />
-        </div>
-        <div className='App-content'>
-          <Navbar />
-          <Routes>
-          {/* arreglar este navigate */}
-            <Route path='/' element={<Navigate to='/talleres' />} />
-            <Route path='/:dynamic' element={<Home search={search} click={click} />} />
-            <Route path='/search' element={<SearchResults />} />
-            <Route path='/resources/:id' element={<Details />} />
-          </Routes>
-        </div>
+        <AppProvider
+          value={{
+            addFavorite,
+            deleteFavorite
+          }}
+        >
+          <div className='App-search'>
+            <Searchbar getInfo={getInfo} getClick={getClick} />
+          </div>
+          <div className='App-content'>
+            <Navbar />
+            <Routes>
+            {/* arreglar este navigate */}
+              <Route path='/' element={<Navigate to='/talleres' />} />
+              <Route path='/:dynamic' element={<Home search={search} click={click} />} />
+              <Route path='/search' element={<SearchResults />} />
+              <Route path='/resources/:id' element={<Details />} />
+            </Routes>
+          </div>
+        </AppProvider>
       </div>
     </div>
   );
--- client/src/components/ListItem/ListItem.js
+++ client/src/components/ListItem/ListItem.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { Link } from 'react-router-dom';
+import AppContext from 'context/AppContext';
 import './ListItem.css'
 import emptyHeart from 'img/favorite_border_24px.png'
-import { Link } from 'react-router-dom';
 
 const ListItem = ({ resource }) => {
   const { image, title, description, id } = resource
+  const [favorite, setFavorite] = useState(false)
+  const { addFavorite, deleteFavorite } = useContext(AppContext)
+
+  const addToFavorites = (id) => {
+    if (!favorite) {
+      addFavorite(id)
+      toggleFavorite()
+    } else {
+      deleteFavorite(id)
+      toggleFavorite()
+    }
+  }
+
+  const toggleFavorite = () => {
+    setFavorite(!favorite)
+  }
 
   return (
     <div className='ListItem-card'>
@@ -14,7 +31,7 @@ const ListItem = ({ resource }) => {
           <h3>{title}</h3>
           <p>{description}</p>
         </Link>
-        <button>
+        <button onClick={() => addToFavorites(id)}>
           <img className='heart' src={emptyHeart} alt="Heart" />
           <span>Favorito</span>
         </button>
